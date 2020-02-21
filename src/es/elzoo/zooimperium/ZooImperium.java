7ea7675f076405386610e7ad9b7706ef59038052
Acunnia <carlosacunagg@gmail.com>
2020-02-21 23:44:35 +0100

    Añadida funcionalidad de picado

--- src/es/elzoo/zooimperium/ZooImperium.java
+++ src/es/elzoo/zooimperium/ZooImperium.java
@@ -1,15 +1,19 @@
 package es.elzoo.zooimperium;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import es.elzoo.zooimperium.eventos.EventosPicar;
 
 public class ZooImperium extends JavaPlugin{
+	public static Plugin pl = null; // DONDE VAMOS A ALMACENAR EL ESTADO DEL SERVIDOR
 	
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new EventosPicar(), this);
-		Bukkit.getLogger().info("El plugin ha sido cargado");	
+		pl = this; // Guardamos el estado del plugin, así tenemos un unico objeto "plugin"
+		
+		getServer().getPluginManager().registerEvents(new EventosPicar(), this); // PREPARA AL SERVIDOR PARA LEER LOS EVENTOS DE EventosPicar
+		Bukkit.getLogger().info("El plugin ha sido cargado");	// MUESTRA POR CONSOLA QUE EL PLUGIN SE HA CARGADO CON EXITO
 	}
 }
--- src/es/elzoo/zooimperium/eventos/EventosPicar.java
+++ src/es/elzoo/zooimperium/eventos/EventosPicar.java
@@ -1,19 +1,42 @@
 package es.elzoo.zooimperium.eventos;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import es.elzoo.zooimperium.ZooImperium;
 
 public class EventosPicar implements Listener {
     @EventHandler
     public static void onBlockBreak(BlockBreakEvent event) {
-        Block block = event.getBlock();
-        if (block.getBlockData().getMaterial().equals(Material.STONE)){
-            event.setCancelled(true);
-//            block.getDrops();
-            block.getLocation().getBlock().setType(Material.BEDROCK);
+        Block block = event.getBlock();											// GUARDAMOS EL BLOQUE Y EL JUGADOR DEL EVENTO
+        Player player = event.getPlayer();
+        
+        ItemStack drop;															// INSTANCIAMOS UN ITEM DE INVENTARIO VACIO
+        
+        if (block.getBlockData().getMaterial().equals(Material.STONE)){			// COMPROBAMOS SI ESTÁ PICANDO PIEDRA
+        	drop = new ItemStack(Material.COBBLESTONE);							// COMO ESTÁ PICANDO PIEDRA, CREAMOS EL ITEM DROP
+			player.getWorld().dropItemNaturally(block.getLocation(), drop);		// DROPEA EL COSO EN LA POSICION DEL BLOQUE
+        	event.setCancelled(true);											// CANCELA EL EVENTO DE PICAR
+            block.getLocation().getBlock().setType(Material.COBBLESTONE);			// COLOCA BEDROCK EN EL BLOQUE PICADO
+            Bukkit.getScheduler().runTaskLater(ZooImperium.pl, () -> {			// PREPARA UNA TAREA FUTURA, NECESITA COMO PARAMETRO EL PLUGIN
+            	if (block.getBlockData().getMaterial().equals(Material.COBBLESTONE)) {
+					block.getLocation().getBlock().setType(Material.STONE);
+				}																// ACCION DE LA TAREA FUTURA (PONER EL BLOQUE ORIGINAL)
+            }, 100L);															// TIEMPO QUE VA A TRANSCURRIR HASTA REALIZAR LA TAREA
+        } else if (block.getBlockData().getMaterial().equals(Material.COBBLESTONE)){			
+        	drop = new ItemStack(Material.COBBLESTONE);							
+			player.getWorld().dropItemNaturally(block.getLocation(), drop);		
+        	event.setCancelled(true);										
+            block.getLocation().getBlock().setType(Material.BEDROCK);	
+            Bukkit.getScheduler().runTaskLater(ZooImperium.pl, () -> {		
+                block.getLocation().getBlock().setType(Material.STONE);	
+            }, 20L);	
         }
         return;
     }
